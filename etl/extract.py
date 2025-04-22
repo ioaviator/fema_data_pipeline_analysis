6408225e2c0a962e3768ab502e70c9db7aafd19d
ioaviator <kingsolomonifeanyi@gmail.com>
2025-04-22 16:49:23 +0100

    pull down data from api

---
+++ etl/extract.py
@@ -0,0 +1,20 @@
+import requests
+
+from ETL.config import URL
+
+
+def extract():
+  
+  try:
+      # FEMA API Endpoint
+    response = requests.get(URL)
+    
+    # Raise an error for HTTP error codes (like 4xx or 5xx)
+    response.raise_for_status()
+
+    data = response.json()['PublicAssistanceFundedProjectsDetails']
+      
+  except requests.exceptions.RequestException as e:
+    print(e)
+  
+  return data
